709f85e6c36bbd9e7b58bf12e939d903a69a8cd1
santinoDV <sdenovi@LAPTOP-61VEQC5G.>
2025-02-27 19:00:00 +0000

    adding jwt dependy injection over service

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49910151046fc4ee2af8f446535bb918a3002cf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e409a4a140df94de24d30e666ff9b504b6437776")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
