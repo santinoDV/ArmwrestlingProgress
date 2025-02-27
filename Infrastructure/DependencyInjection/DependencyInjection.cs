b51b171e9d07b8dba256701c0bd6a70fa883e98d
santinoDV <sdenovi@LAPTOP-61VEQC5G.>
2025-02-27 19:07:15 +0000

    adding jwt json to appsettings.json

--- Infrastructure/DependencyInjection/DependencyInjection.cs
+++ Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿
 
 using System.ComponentModel.Design;
+using System.Text;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.DependencyInjection
 {
@@ -17,6 +20,24 @@ namespace Infrastructure.DependencyInjection
                 options.UseSqlServer(configuration.GetConnectionString("Default"),
                 b => b.MigrationsAssembly(typeof(ServiceContainer).Assembly.FullName)),
                 ServiceLifetime.Scoped);
+
+            services.AddAuthentication(options =>
+            { options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(configuration["Jwt:key"]!))
+                };
+            });
 
             return services;
         }
